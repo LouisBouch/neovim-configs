32e4e698920ea365715a0f6d8a93091b95772ad9
MoyenMedium <louisbouchard@mail.com>
2025-09-06 02:09:36 -0400

    feat: setup lazy.nvim
    Lazy.nvim is now setup and ready to accept new plugins.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Make a method that takse in a list of keybinds and applies them.
+-- like so:
+    --
+    -- [1]: (string) lhs (required)
+    -- [2]: (string|fun()) rhs (optional)
+    -- mode: (string|string[]) mode (optional, defaults to "n")
+    -- ft: (string|string[]) filetype for buffer-local keymaps (optional)
+    -- any other option valid for vim.keymap.set
